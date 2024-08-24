ee2d3df333682953e7544a22c18a335a5c54eb3b
Amnasheikh22 <smilerainpuppy@gmail.com>
2024-08-25 01:37:30 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+let page=document.getElementById('heroo');
+
+})
+let test=document.getElementById('here');
+let cart=document.getElementById('shop');
+
+page.addEventListener('click',()=>{
+window.location.href=("clothing.html");
+})
+test.addEventListener('click',()=>{
+window.location.href=("accessories.html");
+})
+cart.addEventListener('click',()=>{
+window.location.href=("cart.html");
+})
+       
+     
